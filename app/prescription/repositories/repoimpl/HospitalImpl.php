721ce5532e5e72b078541e01050607aef229b733
Baskar <baskar2271@yahoo.com>
2016-11-15 14:49:24 +0530

    Test

--- app/prescription/repositories/repoimpl/HospitalImpl.php
+++ app/prescription/repositories/repoimpl/HospitalImpl.php
@@ -95,7 +95,7 @@ class HospitalImpl implements HospitalInterface{
 
             //$query->distinct()
             $hospitals = $query->get();
-            dd($hospitals);
+            //dd($hospitals);
         }
         catch(QueryException $queryEx)
         {
