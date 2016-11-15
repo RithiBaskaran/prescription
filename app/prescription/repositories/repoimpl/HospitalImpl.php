87eea1723e57ce6ce97b3a958f435be83bfc5892
Baskar <baskar2271@yahoo.com>
2016-11-15 14:26:16 +0530

    Test

--- app/prescription/repositories/repoimpl/HospitalImpl.php
+++ app/prescription/repositories/repoimpl/HospitalImpl.php
@@ -55,6 +55,7 @@ class HospitalImpl implements HospitalInterface{
             $query->where('u.delete_status', '=', 1);
 
             $hospitals = $query->get();
+            dd($hospitals);
         }
         catch(QueryException $queryEx)
         {
@@ -94,7 +95,7 @@ class HospitalImpl implements HospitalInterface{
 
             //$query->distinct()
             $hospitals = $query->get();
-            //dd($hospitals);
+            dd($hospitals);
         }
         catch(QueryException $queryEx)
         {
